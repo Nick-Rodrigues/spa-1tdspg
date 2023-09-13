c633758a7e1726948f5c977ff5935cdb886dd874
Nick-Rodrigues <rm551060@fiap.com.br>
2023-09-13 09:34:34 -0300

    Realização do excluir com useState/useParams.

--- src/routes/ExcluirProduto.jsx
+++ src/routes/ExcluirProduto.jsx
@@ -1,8 +1,49 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { ListaProdutos } from "../components/ListaProdutos";
+import { useState } from "react";
+
 export default function ExcluirProduto() {
 
     document.title = "Excluir Produto";
 
+    const {id} = useParams();
+
+    const navigate = useNavigate();
+
+   const produtoObj = ListaProdutos.filter(item => item.id == id)[0];
+
+   const [produto, setProduto] = useState(produtoObj);
+
+    const handleDelete = ()=>{
+       //Indice que será utilizado para a exclusão do produto na lista.
+      let indice;
+
+      //Localização do índice na lista.
+      ListaProdutos.forEach((item,index)=>{
+          if(item.id == produto.id){
+              indice = index;
+          }
+      });
+
+      //Excluindo produto com splice
+      ListaProdutos.splice(indice,1);
+
+      //Redirecionando o usuário para a lista deprodutos
+      alert("Produto excluído com sucesso!")
+      navigate("/produtos");
+
+    }
+
   return (
-    <div>Excluir Produto</div>
+    <>
+      <h1>Excluir - Produto</h1>
+      <div></div>
+        <h2>{`Deseja realmente excluir o produto ${produto.nome}?`}</h2>
+        <div>
+          <button onClick={handleDelete}>Excluir</button>
+          <button onClick={()=> navigate("/produto")}>Cancelar</button>
+        </div>
+      </div>
+    </>
   )
 }
